0cb94d4702a9355e6d71ed3e0382ed835af31e07
Junaid Aslam <83652548+20sw084@users.noreply.github.com>
2021-10-02 14:55:03 +0500

    Create Insert Data into Tables .sql
    Output will be [6 rows affected].
    as much as columns you inserted in data same number of rows will be affected.

---
+++ Insert Data into Tables .sql
@@ -0,0 +1,9 @@
+use EntranceSystem;
+
+insert into Employees([FirstName],[LastName],[Department],[DateOfJoining],[BasicPay])
+values('Jonny','Rajput','SWE',null,38500.80),
+('Umar','Mahar','SWE',null,38500.89),
+('Ammar','Raza','SWE',null,38500.90),
+('Naveed','Kaimkhani','SWE',null,38500.76),
+('Rameez','Sarki','SWE',null,38500.12),
+('Rasheed','Chandio','CSE',null,38500.23);
