38e0c5ef488d0c557ea45c1bf493a0273ca8d815
Junaid Aslam <83652548+20sw084@users.noreply.github.com>
2021-10-02 14:52:20 +0500

    Create Creating Tables .sql
    You can also create table manually without using code.

---
+++ Creating Tables .sql
@@ -0,0 +1,9 @@
+use EntranceSystem                        --For creating table in given database
+create table Employees(
+	ID int primary key identity(1,1) not null,
+	FirstName nvarchar(50) not null,
+	LastName nvarchar(50) not null,
+	Department nvarchar(50) not null,
+	DateOfJoining datetime,
+	BasicPay float not null,
+);
