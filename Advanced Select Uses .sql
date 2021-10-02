188104aa9a1e5f678aa784d6609ca05f800417de
Junaid Aslam <83652548+20sw084@users.noreply.github.com>
2021-10-02 17:08:53 +0500

    Create Advanced Select Uses .sql
    Select Advanced.

---
+++ Advanced Select Uses .sql
@@ -0,0 +1,30 @@
+Select [FirstName],[LastName]
+From [Employees]
+Where LastName='Rajput';
+
+
+
+Select [FirstName],[LastName]
+From [Employees]
+Where LastName like '%k%';                                    --'%' is a wildcard
+
+
+
+Select [FirstName],[LastName]
+From [Employees]
+Where LastName like 'j%';
+
+
+Select [FirstName],[LastName]
+From [Employees]
+Where LastName like '%student';
+
+
+
+Select [FirstName],[LastName],18 as [Age]                           //This will add a column of age also at the output. But, in actual its not present or saved in database
+From [Employees]
+
+
+Select * from Employees
+Where id=15
+
