a364e0c47b96210e2255e3b3a833ca25ae50d893
Junaid Aslam <83652548+20sw084@users.noreply.github.com>
2021-10-02 14:49:59 +0500

    Update Get Started with SQL .sql
    First SQL File

--- Get Started with SQL .sql
+++ Get Started with SQL .sql
@@ -2,3 +2,5 @@ create database EntranceSystem          --For creating a database using code
 -- Its a Comment in SQL
 Drop database EntranceSystem            --For Deleting existing(EntranceSystem) Database
 
+--Databases can also be made manually without using code
+
