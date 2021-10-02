befa673b92722b1ab146c3279f648c2b1730fc5c
Junaid Aslam <83652548+20sw084@users.noreply.github.com>
2021-10-02 14:48:52 +0500

    Create Get Started with SQL .sql

---
+++ Get Started with SQL .sql
@@ -0,0 +1,4 @@
+create database EntranceSystem          --For creating a database using code
+-- Its a Comment in SQL
+Drop database EntranceSystem            --For Deleting existing(EntranceSystem) Database
+
