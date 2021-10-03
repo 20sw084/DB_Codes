9ec3eee4fafad8ea6af50f7d4beff54b34d447ed
Junaid Aslam <83652548+20sw084@users.noreply.github.com>
2021-10-03 08:52:34 +0500

    Create Delete Data from Table using SQL Script .sql
    Delete Uses

---
+++ Delete Data from Table using SQL Script .sql
@@ -0,0 +1,8 @@
+delete from Employees where id=7
+
+delete from Employees where FirstName='ABC' and LastName='SARKI'
+
+delete from Employees where FirstName='ABC' or LastName='SARKI'
+
+delete from Employees where FirstName like '%j%' or LastName like '%k%'
+
